63b67bb95c1bd9b4b980d3a250bb813600aa0b52
Jack Carver <jrcman13@gmail.com>
2023-05-25 18:41:06 -0500

    Fixed Checkbox descriptions not showing up

--- src/CompletedView/CompletedViewJsonCheckbox.js
+++ src/CompletedView/CompletedViewJsonCheckbox.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import axios from 'axios';
 import EditTaskButton from '../EditTask/EditTaskButton';
+import { ListGroup, Collapse } from 'react-bootstrap';
 
 export default class CompletedViewJsonCheckbox extends React.Component {
   constructor(props) {
@@ -11,6 +12,11 @@ export default class CompletedViewJsonCheckbox extends React.Component {
       showEdit: false,
     };
     this.changeDisplay = this.changeDisplay.bind(this);
+    this.setOpen = this.setOpen.bind(this);
+  }
+
+  setOpen(value) {
+    this.setState({ open: value });
   }
 
   async changeDisplay(event) {
@@ -40,16 +46,25 @@ export default class CompletedViewJsonCheckbox extends React.Component {
     return (
       <div style={{ display }} onMouseEnter={this.handleHover} onMouseLeave={this.handleMouseLeave}>
         <div style={{display: 'flex', justifyContent: 'space-between'}}>
-          <div>
-            <Form.Check
-              className='checkbox'
-              type='checkbox'
-              label={this.props.label}
-              defaultChecked={this.props.checked}
-              onChange={(event) => this.changeDisplay(event)}
-            />
-            <p>Due: {this.props.deadline}</p>
-          </div>
+          <ListGroup>
+            <div style={{textAlign: 'left', padding: '0', margin: '0'}} onClick={() => this.setOpen(!this.state.open)} aria-controls='collapse1' aria-expanded={this.state.open}>
+              <Form.Check
+                className='checkbox'
+                type='checkbox'
+                label={this.props.label}
+                defaultChecked={this.props.checked}
+                onChange={(event) => this.changeDisplay(event)}
+              />
+              <p>Due: {this.props.deadline}</p>
+            </div>
+            <Collapse in={this.state.open}>
+              <div id='collapse1' style={{padding: '0', margin: '0'}}>
+                <ListGroup.Item className='text-left' style={{border: 'none', backgroundColor: `var(--${this.props.category})`, padding: '0', margin: '0'}}>
+                  <p>{this.props.description}</p>
+                </ListGroup.Item>
+              </div>
+            </Collapse>
+          </ListGroup>
           {this.state.showEdit && 
             <EditTaskButton flaskUrl={this.props.flaskUrl} taskId={this.props.taskId} hideEditButton={this.handleMouseLeave} />
           }
