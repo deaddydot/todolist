1baa9681dcb4a60bba7bc9f6385462de7029251b
Jack Carver <jrcman13@gmail.com>
2023-06-11 01:28:38 -0500

    Resolved web errors and removed hard-coded colors

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(  
   <div style={{display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
     <React.StrictMode>
       <Auth0Provider
         domain="dev-yjpxjgkbim0ygj8c.us.auth0.com"
         clientId="ixHfvzExgH69wz7sfTToWwvoH9xATg4t"
-        redirectUri={window.location.origin}
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
       >
         <App />
       </Auth0Provider>
     </React.StrictMode>
-  </div>,
-  document.getElementById("root")
+  </div>
 );
