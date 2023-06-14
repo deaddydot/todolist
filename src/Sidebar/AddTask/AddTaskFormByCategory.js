2ac80576b5b14c81275db767bcd4fb36cbbc83d9
Jack Carver <jrcman13@gmail.com>
2023-06-13 22:42:50 -0500

    Added Add Task Button to Categories
    Left in sidebar as well, as I think this works better for the calendar view

---
+++ src/Sidebar/AddTask/AddTaskFormByCategory.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import UserCategories from '../../UserCategories';
+
+export default class AddTaskFormByCategory extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      description: "",
+      deadline: "",
+      category_id: "",
+      user_id: 0,    // pass this down by props
+    };
+  }
+
+  componentDidMount() {
+    // fetch categories from Flask API when component mounts
+    axios.get(`${this.props.flaskUrl}/categories/${this.state.user_id}`)
+      .then(response => {
+        const categories = response.data;
+        const currentCategory = categories.find(category => category.name === this.props.category);
+        this.setState({ category_id: currentCategory.id });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { title, description, deadline, category_id } = this.state;
+    
+    const datetimeObject = new Date(deadline);
+    const formattedDatetime = `${datetimeObject.getMonth()+1}-${datetimeObject.getDate()}-${datetimeObject.getFullYear()} ${datetimeObject.getHours()}:${datetimeObject.getMinutes()}:${datetimeObject.getSeconds()}`;
+
+    const data = {
+      title,
+      description,
+      formattedDatetime,
+      category_id,
+    };
+    try {
+      const response = await axios.post(`${this.props.flaskUrl}/tasks/${this.state.user_id}`, data);
+
+      this.props.onTaskAdded(response.data.category_id);
+    } catch (error) {
+      console.error(error);
+    }
+
+    this.props.modalOpen();
+
+    window.location.reload();
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { title, description, deadline, category_id } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit} style={{display: 'flex', flexDirection: 'column'}}>
+        <label>
+          Title: 
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Description: 
+          <input
+            type="text"
+            name="description"
+            value={description}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Deadline: 
+          <input
+            type="datetime-local"
+            id="datetime"
+            name="deadline"
+            value={deadline}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Category: {this.props.category}
+        </label>
+        <Button style={{backgroundColor: 'blue', border: 'none'}} type="submit">Submit</Button>
+      </form>
+    );
+  }
+}
