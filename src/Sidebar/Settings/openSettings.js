5686ad005835b3a07d4c8c1dd9078230d3a1956a
Hudson Bailey <Baileyhuds@gmail.com>
2023-10-05 18:11:24 -0500

    Added settings for task bold hover Still dont know how it works

---
+++ src/Sidebar/Settings/openSettings.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "./Settings.css";
+import SettingsForm from "./SettingsForm";
+import Button from 'react-bootstrap/Button';
+
+
+
+
+
+
+export default class SettingsButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { displayModal: false };
+  }
+
+  componentDidMount() {
+    document.addEventListener("click", this.handleOutsideClick, true);
+  }
+  
+  componentWillUnmount() {
+    document.removeEventListener("click", this.handleOutsideClick, true);
+  }
+  
+
+  handleOutsideClick = (event) => {
+    const modalContent = document.querySelector(".modal-content");
+    if (modalContent && !modalContent.contains(event.target)) {
+      this.modalClose();
+    }
+  };
+  
+
+  modalOpen = () => {
+    this.setState({ displayModal: true });
+    document.body.style.overflow = 'hidden';
+  };
+
+  modalClose = () => {
+    this.setState({ displayModal: false });
+    document.body.style.overflow = 'hidden';
+  };
+
+  render() {
+    const buttonStyle = {
+      height: 100,
+      width: "100%",
+      border: "none",
+      backgroundColor: this.props.nightMode ? '#282A3A' : '#C69749', // Set background color based on nightMode
+      color: this.props.nightMode ? 'white' : 'black', // Assuming you want the text color to be white
+    };
+    return (
+      <>
+        <button
+          id="myBtn"
+          onClick={this.modalOpen}
+          style={buttonStyle}
+          // margin={50}
+        >
+          <h2>Settings</h2>
+        </button>
+  
+        {this.state.displayModal && (
+          <SettingsForm
+            flaskUrl={this.props.flaskUrl}
+            modalClose={this.modalClose}  // Pass modalClose instead of modalOpen
+            userId={this.props.userId}
+            nightMode={this.props.nightMode}
+          />
+        )}
+      </>
+    );
+  }
+  
+}
